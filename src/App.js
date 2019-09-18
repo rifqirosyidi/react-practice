fdcda02fdb1875c708c60400d9c4e35db7a1901f
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-18 17:16:55 +0700

    passing data to components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function Body() {
+function Body(props) {
   return (
-    <p className='app-intro'>This is Body Tags</p>
+    <div>
+      <p>{props.text}</p>
+      <p>{props.text2}</p>
+      <p>The Result of 3 + 5 is {props.myFunc(3,5)}</p>
+    </div>
   )
 }
 
@@ -16,8 +20,15 @@ class Header extends Component {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi
+          {this.props.title}
         </p>
+        <div>{this.props.myObj.a}</div>
+        {/* You cant just display the object like this.props.myObj it will return an error, 
+        you must stringify it first */}
+        <div>{JSON.stringify(this.props.myObj)}</div>
+        <div>{this.props.myArr[1]}</div>
+        <div>{this.props.myFunc(10, 25)}</div>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -32,10 +43,30 @@ class Header extends Component {
 }
 
 function App() {
+  const add = (a, b) => {
+    return a + b;
+  }
+
   return (
     <div className="App">
-      <Header />
-      <Body />
+      <Header 
+        title={"Hello World"} 
+        num={5} 
+        myArr={[
+          "Array One",
+          "Array Two"
+        ]}
+        myObj={{ 
+          a: "Object a", 
+          b: "Object b"
+        }}
+        myFunc={add} 
+      />
+
+      <Body 
+        text={"Lorem Ipsum Dolor Sit Amet."} 
+        text2={"Text Two"}
+        myFunc={add} />
     </div>
   );
 }
