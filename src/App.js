51aa87517345851ebe076be08ae19b0251676755
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-18 21:05:51 +0700

    image slider visible

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import ImageSlider from './components/ImageSlider';
+import Counter from './components/Counter';
 
 
-function App() {
+class App extends Component {
 
-  return (
-    <div className="App">
-      <ImageSlider />
-    </div>
-  );
+  state = {
+    isVisible: true
+  }
+
+  render(){
+
+    const buttonText = this.state.isVisible ? "Hide" : "Show"
+    const slider = this.state.isVisible ? <ImageSlider /> :  <Counter initialVal={0} />
+
+    return (
+      <div className="App">
+        {slider}
+        <button 
+          onClick={ () => {this.setState({ isVisible: !this.state.isVisible})} }
+        >
+          {buttonText}
+        </button>
+      </div>
+    );  
+  }
 }
 
 export default App;
