662b68f2f336aaafc5a35c754206aa0c9a7bba47
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-19 21:46:42 +0700

    lifting up state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, {Component} from 'react';
 import './App.css';
-import FetchRandomUser from './components/FetchRandomUser';
+import Counter from './components/Counter';
 
 
 class App extends Component {
+
+  state = {
+    count: 0
+  }
+
+  BtnIncrement = () => {
+      this.setState({
+          count: this.state.count + 1
+      })
+  }
+
+  BtnDecrement = () => {
+      this.setState({
+          count: this.state.count - 1
+      })
+  }
+
   render(){
     return(
       <div className="App">
-        <FetchRandomUser />
+
+        {/* So now you can have two sync components */}
+        <Counter 
+          count={this.state.count} 
+          increment={this.BtnIncrement}
+          decrement={this.BtnDecrement} 
+        />
+
+        <Counter 
+          count={this.state.count} 
+          increment={this.BtnIncrement}
+          decrement={this.BtnDecrement} 
+        />
       </div>
     )
   }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,37 +1,12 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-
-    state = {
-        count: 0
-    }
-
-    componentWillUnmount() {
-        console.log("Unmount")
-    }
-
-    componentDidMount(){
-        console.log("Mounting")
-    }
-    
-    BtnIncrement = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
-
-    BtnDecrement = () => {
-        this.setState({
-            count: this.state.count - 1
-        })
-    }
-
     render() {
         return (
             <div>
-                <p>Counter: {this.state.count}</p>
-                <button onClick={this.BtnIncrement} >Increment</button>
-                <button onClick={this.BtnDecrement} >Decrement</button>
+                <p>Counter: {this.props.count}</p>
+                <button onClick={this.props.increment} >Increment</button>
+                <button onClick={this.props.decrement} >Decrement</button>
             </div>
         );
     }
